c8a04516272f8652c9ac55547c97e588c985aab8
Mahmod <mahmoud@myndLift.com>
2017-11-02 18:20:31 +0200

    First commit

---
+++ my-library/src/main/java/com/myndlift/my_library/Debugy.java
@@ -0,0 +1,13 @@
+package com.myndlift.my_library;
+
+import android.util.Log;
+
+/**
+ * Created by Mahmod on 02/11/2017.
+ */
+
+public class Debugy {
+    public static void LogyDogy(){
+        Log.i("Debugy","LOGYDOGY");
+    }
+}
